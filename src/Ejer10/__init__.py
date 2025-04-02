da845173ebfacb54b9a85596ce46f49889d5ddc8
Luca José Rivas <lucajrivas@gmail.com>
2025-04-02 19:00:54 -0300

    agrego comentarios

--- src/Ejer10/__init__.py
+++ src/Ejer10/__init__.py
@@ -2,9 +2,14 @@ def calcular_puntaje(kills, assists, deaths):
     return (kills * 3) + (assists) - (1 if deaths else 0)
 
 def generar_ranking(estadisticas_gral):
+    # la funcion sorted se utiliza para ordenar una secuencia
+    # .items() convierte mi diccionario en una lista de tuplas, separando (juador,estadistica)
+    # key sera el criterio de ordenacion, donde x es una tupla y x[1]['points'] referenciara a las estadisticas/puntos
+    # reverse hace que se ordenen de mayor a menor
     jugadores_ordenados = sorted(estadisticas_gral.items(), key=lambda x: x[1]['points'], reverse=True)
     print("Jugador   Kills   Asistencias   Muertes   MVPs   Puntos")
     print("--------------------------------------------------------")
+    # no aplico items en este jugadores_ordenados porque ya estoy trabajando con valores desmpaquetados
     for jugador, estadisticas in jugadores_ordenados:
         print(f"{jugador:<9} {estadisticas['kills']:<7} {estadisticas['assists']:<13} {estadisticas['deaths']:<9} {estadisticas['mvp']:<6} {estadisticas['points']:<5}")
     print("--------------------------------------------------------")
--- src/__init__.py
+++ src/__init__.py
@@ -2,12 +2,19 @@ from collections import defaultdict
 from src.Ejer10 import calcular_puntaje, generar_ranking
 
 def rondas(rounds):
+    #defino con defaultdict un diccionario que trabajara sin riesgo de que no exista una clave
+    #con esto inicializo mis valores en 0 para poder trabajarlos sin riesgo en mi primera iteracion 
+    #asi puedo ir sumando estadisticas totales
     estadisticas_jugador = defaultdict(lambda: {'kills': 0, 'assists': 0, 'deaths': 0, 'mvp': 0, 'points': 0})
     
+    #utilizo enumerate para llevar un conteo de las rondas desde 1
     for i, datos_de_ronda in enumerate(rounds, 1):
+        #defino diccionarios comunes
+        #que ire creando con la informacion pertinente en CADA ronda
         puntajes_de_ronda = {}
         estadisticas_ronda = {} 
         
+        #utilizo items para desempaquetar el diccionario y asignarlo a las variables 
         for jugador, estadisticas in datos_de_ronda.items():
             kills, assists, deaths = estadisticas['kills'], estadisticas['assists'], estadisticas['deaths']
             puntaje = calcular_puntaje(kills, assists, deaths)
@@ -20,6 +27,9 @@ def rondas(rounds):
             estadisticas_jugador[jugador]['deaths'] += int(deaths)
             estadisticas_jugador[jugador]['points'] += puntaje
 
+        # la funcion max utiliza el iterable con los puntajes de la ronda
+        # pero utiliza el key=puntajes_de_ronda.get para que el criterio de comparacion sean los valores de las claves
+        # al ponerlo de esta manera compararemos valores, pero devolveremos claves   
         mvp = max(puntajes_de_ronda, key=puntajes_de_ronda.get)
         estadisticas_jugador[mvp]['mvp'] += 1
         estadisticas_ronda[mvp]['mvp'] = estadisticas_jugador[mvp]['mvp']
